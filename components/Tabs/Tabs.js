6f3d95d81d50d39529ecaf58cc0e089a4550feea
Wes-White <wesleyrwhite86@gmail.com>
2019-04-18 20:16:18 -0700

    completed tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -44,12 +44,13 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items = document.querySelectorAll('.tabs .tabs-item')
+    const items = document.querySelectorAll('.tabs-item')
 
     // Remove the class "tabs-item-selected" from each element
+    items.forEach(item => item.classList.remove('tabs-item-selected'))
     
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
